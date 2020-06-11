2c642cb4143b73d9f101af3cdfc8ce597c3d5ca1
Ritik Gupta <guptaritik349@gmail.com>
2020-06-11 19:53:09 +0530

    Added Authentication

--- app.js
+++ app.js
@@ -9,6 +9,29 @@ const leaderRouter = require("./routes/leaderRouter");
 const promotions = require("./models/promotions");
 const leader = require("./models/leaders");
 
+function auth(req, res, next) {
+  var authHeader = req.headers.authorization;
+  if (!authHeader) {
+    var err = new Error("You are not authenticated");
+    res.setHeader("WWW-Authenticate", "Basic");
+    err.status = 401;
+    next(err);
+  }
+  var auth = new Buffer(authHeader.split(" ")[1], "base64").toString().split(":");
+  var username = auth[0];
+  var password = auth[1];
+  if (username === "admin" && password === "password") {
+    next();
+  } else {
+    var err = new Error("You are not authenticated");
+    res.setHeader("WWW-Authenticate", "Basic");
+    err.status = 401;
+    next(err);
+  }
+}
+
+app.use(auth);
+
 app.use("/promotions", promoRouter);
 app.use("/leaders", leaderRouter);
 
