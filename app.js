ea485ed252681e9763968291551d12df5dec70af
Ritik Gupta <guptaritik349@gmail.com>
2020-06-14 21:16:35 +0530

    Updated File Upload

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const router = require("./routes/users");
 const dishRouter = require("./routes/dishRouter");
 const promoRouter = require("./routes/promoRouter");
 const leaderRouter = require("./routes/leaderRouter");
+const uploadRouter = require("./routes/uploadRouter");
 
 const User = require("./models/user");
 const dishes = require("./models/dishes");
@@ -34,6 +35,7 @@ app.use("/users", router);
 app.use("/dishes", dishRouter);
 app.use("/promotions", promoRouter);
 app.use("/leaders", leaderRouter);
+app.use("/imageUpload", uploadRouter);
 
 const url = config.mongoUrl;
 
